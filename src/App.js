0e8d0d532f96cd860f1562e8be18429eb62a20c4
markporo <markporo@yahoo.com>
2021-05-30 23:46:34 -0400

    connected api to project and successfully retrieved data,  Styled using styled components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import './App.css';
+import Character from "./components/Character";
+import axios from "axios";
+import styled from "styled-components";
+
+// footer styled
+const ContainerStyled = styled.div`
+margin-bottom: 50px;
+h1{
+  margin-bottom: 25px;
+}
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [Characters, SetCharacters] = useState([]);
+
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people/")
+      .then(res => {
+        SetCharacters(res.data);
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <ContainerStyled>
+      <div className="App">
+        <h1 className="Header">Star Wars Characters</h1>
+        {
+          Characters.map(eachCharacter => {
+            return <Character name={eachCharacter.name} height={eachCharacter.height} films={eachCharacter.films} birthdate={eachCharacter.birth_year} hair={eachCharacter.hair_color} skin={eachCharacter.skin_color} home={eachCharacter.homeworld} eyes={eachCharacter.eye_color} />
+          })
+        }
+      </div>
+    </ContainerStyled>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,80 @@
 // Write your Character component here
+import axios from "axios";
+// import { createResponseComposition } from "msw";
+import React, { useEffect, useState } from "react";
+import styled from 'styled-components';
+
+//styled Div
+const StyledDivContainer = styled.div`
+
+background-color: whiteSmoke;
+width: 70%;
+margin: 0 auto;
+padding: 15px;
+border: 2px solid black;
+
+section {
+    text-align: left;
+    margin-left: 40px;
+    ${'' /* display: none; */}
+}
+
+span {
+    font-weight: bold;
+}
+
+header {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-around;
+    align-items: center;
+    margin-left: 40px;
+}
+
+button {
+    height: 40px;
+    background-color: lightblue;
+     &:hover{
+     transform: scale(1.05);
+     background-color: black;
+     color: lightblue;
+     }
+}
+
+h2 {
+    font-size: 2rem;
+}
+`
+
+
+
+
+export default function Character({ ...props }) {
+
+    const [homeRef, setHomeRef] = useState();
+
+    useEffect(() => {
+        axios.get(props.home)
+            .then(res => {
+                console.log(res.data.name)
+                setHomeRef(res.data.name);
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    })
+
+    return (<StyledDivContainer>
+        <header><h2>{props.name}</h2><button> Character Stats</button></header>
+        <section>
+            <p><span>From:</span> {homeRef}</p>
+            <p><span>Birthday:</span> {props.birthdate}</p>
+            <p><span>Movies Featured in:</span> {props.films.map(eachFilm => { return `${eachFilm}, ` })}</p>
+            <p><span>Height:</span> {props.height} cm</p>
+            <p><span>Skin Type:</span> {props.skin}</p>
+            <p><span>Hair Color:</span> {props.hair}</p>
+            <p><span>Eye Color:</span> {props.eyes}</p>
+        </section>
+
+    </StyledDivContainer>)
+}
